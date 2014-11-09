766e2ca437a2b112da82decbc0856a3785d1bc2a
Rocky Assad <rassad@gbhem.org>
2014-11-08 23:45:20 -0600

    add github wrapper

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 #!/usr/bin/env node
 
-//var cron = require('./lib/cron');
 var CronJob = require('cron').CronJob;
+var Github = require('./lib/github');
+var config = require('./config.json');
 
-new CronJob('* * * * * *', function() {
-	console.log('You will see this message every second');
+var gh = new Github();
+
+gh.auth(config.github_user, config.github_pass);
+
+new CronJob('* * * * * *', function () {
+	console.log('githubbing');
+	gh.getOpenIssues(config.github_user, config.github_repo, function (err, issues) {
+		if (err) {
+			console.log(err);
+			return;
+		}
+		console.log(issues.length);
+	});
 }, null, true, 'America/Chicago');
 
+/*
 var argv = require('minimist')(process.argv.slice(2));
 console.dir(argv);
+*/
