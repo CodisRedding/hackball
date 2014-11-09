5c69a5319cdb065fdfc8551054728727ad3c8f5f
Rocky Assad <rassad@gbhem.org>
2014-11-08 20:57:52 -0600

    add cron and github

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 #!/usr/bin/env node
 
+//var cron = require('./lib/cron');
+var CronJob = require('cron').CronJob;
+
+new CronJob('* * * * * *', function() {
+	console.log('You will see this message every second');
+}, null, true, 'America/Chicago');
+
 var argv = require('minimist')(process.argv.slice(2));
 console.dir(argv);
